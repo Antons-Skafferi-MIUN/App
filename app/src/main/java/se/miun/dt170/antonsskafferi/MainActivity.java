a164762dc3d6af7e3fbe25e840eb8cf9947a2005
Simon <sibe1301@student.miun.se>
2020-02-14 17:12:49 +0100

    added some basic UI elements

--- app/src/main/java/se/miun/dt170/antonsskafferi/MainActivity.java
+++ app/src/main/java/se/miun/dt170/antonsskafferi/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import se.miun.dt170.antonsskafferi.activity.KitchenActivity;
 import se.miun.dt170.antonsskafferi.activity.WaiterActivity;
@@ -13,25 +14,29 @@ import se.miun.dt170.antonsskafferi.activity.WaiterActivity;
 public class MainActivity extends AppCompatActivity {
 
     AppCompatActivity self = this;
-
+    private CheckBox waiterCheckBox;
+    private CheckBox kitchenCheckBox;
+    private Button enterButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button kitchenButton = findViewById(R.id.kitchenButton);
-        Button waiterButton = findViewById(R.id.waiterButton);
+        enterButton = findViewById(R.id.enterButton);
+        waiterCheckBox = findViewById(R.id.waiterCheckBox);
+        kitchenCheckBox = findViewById(R.id.kitchenCheckBox);
 
-        kitchenButton.setOnClickListener(new View.OnClickListener() {
+        enterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(self , KitchenActivity.class));
-            }
-        });
-        waiterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(self , WaiterActivity.class));
+                if(waiterCheckBox.isChecked() && !kitchenCheckBox.isChecked()){
+                    startActivity(new Intent(self , WaiterActivity.class));
+                    waiterCheckBox.setChecked(false);
+                }
+                else if(kitchenCheckBox.isChecked() && !waiterCheckBox.isChecked()){
+                    startActivity(new Intent(self , KitchenActivity.class));
+                }
+
             }
         });
     }
