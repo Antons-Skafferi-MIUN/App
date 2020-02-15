1345063a53f151b2c064ece3ef343f0c8e5f5198
Simon <sibe1301@student.miun.se>
2020-02-15 11:55:42 +0100

    minor fixes to UI

--- app/src/main/java/se/miun/dt170/antonsskafferi/MainActivity.java
+++ app/src/main/java/se/miun/dt170/antonsskafferi/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import se.miun.dt170.antonsskafferi.activity.KitchenActivity;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private Button enterButton;
     private TextView waiterTextView;
     private TextView kitchenTextView;
+    private EditText inputEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         waiterCheckBox = findViewById(R.id.waiterCheckBox);
         kitchenCheckBox = findViewById(R.id.kitchenCheckBox);
         waiterTextView = findViewById(R.id.waiterTextView);
+        kitchenTextView = findViewById(R.id.kitchenTextView);
+        inputEditText = findViewById(R.id.inputBox);
 
         waiterCheckBox.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,15 +49,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        kitchenCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!kitchenCheckBox.isChecked()){
+                    kitchenTextView.setTextColor(Color.parseColor("#FFFFFF"));
+
+                }
+                else {
+                    kitchenTextView.setTextColor(Color.parseColor("#00FF00"));
+                }
+            }
+        });
+
         enterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(waiterCheckBox.isChecked() && !kitchenCheckBox.isChecked()){
-                    startActivity(new Intent(self , WaiterActivity.class));
                     waiterCheckBox.setChecked(false);
+                    waiterTextView.setTextColor(Color.parseColor("#FFFFFF"));
+                    Intent intent = new Intent(self, WaiterActivity.class);
+                    intent.putExtra("DISPLAY_NAME", "Asd"); // NEEDS TO BE ASYNC
+
+                    startActivity(intent);
+
                 }
                 else if(kitchenCheckBox.isChecked() && !waiterCheckBox.isChecked()){
+                    kitchenCheckBox.setChecked(false);
+                    kitchenTextView.setTextColor(Color.parseColor("#FFFFFF"));
                     startActivity(new Intent(self , KitchenActivity.class));
+
                 }
 
             }
--- app/src/main/java/se/miun/dt170/antonsskafferi/activity/WaiterActivity.java
+++ app/src/main/java/se/miun/dt170/antonsskafferi/activity/WaiterActivity.java
@@ -2,15 +2,22 @@ package se.miun.dt170.antonsskafferi.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import se.miun.dt170.antonsskafferi.R;
 
 public class WaiterActivity extends AppCompatActivity {
+    private TextView name;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiter);
+        intent = getIntent();
+        name = findViewById(R.id.displayName);
+        name.setText("Hello " + intent.getStringExtra("DISPLAY_NAME"));
     }
 }
