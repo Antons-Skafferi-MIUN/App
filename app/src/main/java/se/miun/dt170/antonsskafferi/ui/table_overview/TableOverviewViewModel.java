a736af85e1a4bf5e65600ff34c693cd31f4c07dd
Viktor Rosvall <virosen@yahoo.se>
2020-02-18 08:52:39 +0100

    changed waiter overview to use fullscreen fragment with nav_graph

---
+++ app/src/main/java/se/miun/dt170/antonsskafferi/ui/table_overview/TableOverviewViewModel.java
@@ -0,0 +1,7 @@
+package se.miun.dt170.antonsskafferi.ui.table_overview;
+
+import androidx.lifecycle.ViewModel;
+
+public class TableOverviewViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
