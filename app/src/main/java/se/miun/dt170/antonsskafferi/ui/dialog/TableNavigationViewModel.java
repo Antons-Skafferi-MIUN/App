d2a9531641c64b7a134554070215b6043a60ed52
Simon Bertlin <sibe1301@student.miun.se>
2020-02-18 12:19:24 +0100

    added dialog fragment

---
+++ app/src/main/java/se/miun/dt170/antonsskafferi/ui/dialog/TableNavigationViewModel.java
@@ -0,0 +1,7 @@
+package se.miun.dt170.antonsskafferi.ui.dialog;
+
+import androidx.lifecycle.ViewModel;
+
+public class TableNavigationViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
