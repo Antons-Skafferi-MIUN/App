873dab7fb7a64f673435f192cf71e88faf59060c
Simon Bertlin <sibe1301@student.miun.se>
2020-02-20 15:49:13 +0100

    added food dialog

---
+++ app/src/main/java/se/miun/dt170/antonsskafferi/ui/dialog/FoodEditDialog.java
@@ -0,0 +1,47 @@
+package se.miun.dt170.antonsskafferi.ui.dialog;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import se.miun.dt170.antonsskafferi.R;
+
+public class FoodEditDialog extends DialogFragment {
+
+    private FoodEditDialogViewModel mViewModel;
+    private View dialogView;
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        LayoutInflater layoutInflater = requireActivity().getLayoutInflater();
+        dialogView = layoutInflater.inflate(R.layout.food_edit_dialog_fragment, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(dialogView);
+        return builder.create();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        dialogView = view;
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(FoodEditDialogViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
