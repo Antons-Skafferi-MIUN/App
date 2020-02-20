1ec7cef98d9b39f10c928a5fa824d031699866fe
Simon Bertlin <sibe1301@student.miun.se>
2020-02-20 12:42:01 +0100

    fixed dialog button

--- app/src/main/java/se/miun/dt170/antonsskafferi/ui/dialog/TableDialogFragment.java
+++ app/src/main/java/se/miun/dt170/antonsskafferi/ui/dialog/TableDialogFragment.java
@@ -3,6 +3,7 @@ package se.miun.dt170.antonsskafferi.ui.dialog;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 
@@ -14,14 +15,15 @@ import android.view.View;
 import android.widget.Button;
 
 import se.miun.dt170.antonsskafferi.R;
+import se.miun.dt170.antonsskafferi.ui.table_overview.TableOverviewViewModel;
 
 public class TableDialogFragment extends DialogFragment {
     private Button openOrderButton;
     private View dialogView;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         LayoutInflater layoutInflater = requireActivity().getLayoutInflater();
-
         dialogView = layoutInflater.inflate(R.layout.table_dialog_fragment, null);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setView(dialogView);
@@ -29,19 +31,25 @@ public class TableDialogFragment extends DialogFragment {
         return builder.create();
     }
 
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        openOrderButton = view.findViewById(R.id.openOrderButton);
-        openOrderButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openOrderButton.setText("ASDASDASDASDASD");
-                NavDirections action = TableDialogFragmentDirections.actionTableDialogFragmentToOrderOverviewFragment();
-                Navigation.findNavController(getView()).navigate(action);
-            }
-        });
+        dialogView = view;
 
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        openOrderButton = dialogView.findViewById(R.id.openOrderButton);
+        openOrderButton.setOnClickListener(v -> {
+            NavDirections action = TableDialogFragmentDirections.actionTableDialogFragmentToOrderOverviewFragment();
+            Navigation.findNavController(getParentFragment().getView()).navigate(action);
+        });
+    }
+
+
 }
+
+
