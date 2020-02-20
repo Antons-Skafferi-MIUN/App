3f8d96039154812d0150ad4bb139e776ffad279e
Viktor Rosvall <virosen@yahoo.se>
2020-02-20 08:48:12 +0100

    Added table button fragment

---
+++ app/src/main/java/se/miun/dt170/antonsskafferi/ui/table_overview/table_button/TableButtonFragment.java
@@ -0,0 +1,38 @@
+package se.miun.dt170.antonsskafferi.ui.table_overview.table_button;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import se.miun.dt170.antonsskafferi.R;
+
+public class TableButtonFragment extends Fragment {
+
+    private TableButtonViewModel mViewModel;
+
+    public static TableButtonFragment newInstance() {
+        return new TableButtonFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.table_button_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(TableButtonViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/se/miun/dt170/antonsskafferi/ui/table_overview/table_button/TableButtonViewModel.java
@@ -0,0 +1,7 @@
+package se.miun.dt170.antonsskafferi.ui.table_overview.table_button;
+
+import androidx.lifecycle.ViewModel;
+
+public class TableButtonViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
