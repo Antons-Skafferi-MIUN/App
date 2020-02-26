a64bf7a6d4227f4a207fb20448c7b749456b3461
mariaolsson92 <maria.olsson92@hotmail.se>
2020-02-26 08:51:01 +0100

    Converted menu item fragment to custom cardview

---
+++ app/src/main/java/se/miun/dt170/antonsskafferi/ui/order_overview/order_overview_menu_container/OrderOverviewMenuContainerViewModel.java
@@ -0,0 +1,7 @@
+package se.miun.dt170.antonsskafferi.ui.order_overview.order_overview_menu_container;
+
+import androidx.lifecycle.ViewModel;
+
+public class OrderOverviewMenuContainerViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/se/miun/dt170/antonsskafferi/ui/order_overview/order_overview_menu_item_view/OrderOverviewMenuItemView.java
@@ -0,0 +1,6 @@
+package se.miun.dt170.antonsskafferi.ui.order_overview.order_overview_menu_item;
+
+import androidx.cardview.widget.CardView;
+
+public class OrderOverviewMenuItem extends CardView {
+}
