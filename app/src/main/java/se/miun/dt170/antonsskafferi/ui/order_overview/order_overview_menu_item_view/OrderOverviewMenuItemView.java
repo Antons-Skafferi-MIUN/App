23358e99bf18b06c1300ea45a1f58363427747b0
mariaolsson92 <maria.olsson92@hotmail.se>
2020-02-26 10:17:51 +0100

    Created a view for showing different menu categories

--- app/src/main/java/se/miun/dt170/antonsskafferi/ui/order_overview/order_overview_menu_item_view/OrderOverviewMenuItemView.java
+++ app/src/main/java/se/miun/dt170/antonsskafferi/ui/order_overview/order_overview_menu_item_view/OrderOverviewMenuItemView.java
@@ -1,6 +1,18 @@
-package se.miun.dt170.antonsskafferi.ui.order_overview.order_overview_menu_item;
+package se.miun.dt170.antonsskafferi.ui.order_overview.order_overview_menu_item_view;
 
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+
+import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 
-public class OrderOverviewMenuItem extends CardView {
+import se.miun.dt170.antonsskafferi.R;
+
+public class OrderOverviewMenuItemView extends CardView {
+    public OrderOverviewMenuItemView(@NonNull Context context) {
+        super(context);
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        layoutInflater.inflate(R.layout.order_overview_menu_container_fragment, this, true);
+    }
 }
