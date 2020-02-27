413ded91f4ef09c1446d9ee81a6a3c88a0e99933
Viktor Rosvall <virosen@yahoo.se>
2020-02-27 16:19:32 +0100

    Fixed kitchen dynamic adding bong lists

---
+++ app/src/main/java/se/miun/dt170/antonsskafferi/ui/bong/BongListView.java
@@ -0,0 +1,30 @@
+package se.miun.dt170.antonsskafferi.ui.bong;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import se.miun.dt170.antonsskafferi.R;
+import se.miun.dt170.antonsskafferi.data.Item;
+
+/**
+ * This is the container for the list of {@link BongItemView}.
+ * Displayed to the right in the order {@link se.miun.dt170.antonsskafferi.ui.order_overview.OrderOverviewFragment}.
+ * Displayed in the {@link se.miun.dt170.antonsskafferi.activity.KitchenActivity}.
+ */
+public class BongListView extends LinearLayout {
+
+    public BongListView(Context context) {
+        super(context);
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View inflate = inflater.inflate(R.layout.bong_list_view, this, true);
+
+        setOrientation(VERTICAL); // VERY IMPORTANT
+    }
+}
