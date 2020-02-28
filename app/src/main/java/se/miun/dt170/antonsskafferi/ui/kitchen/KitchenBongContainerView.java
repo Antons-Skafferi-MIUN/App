aed360a33104f15eb74ec7aedb28217065171b06
Viktor Rosvall <virosen@yahoo.se>
2020-02-28 13:02:07 +0100

    changed kitchen activity to spawm bong containers. The bong containers will fill bong lists with items

--- app/src/main/java/se/miun/dt170/antonsskafferi/ui/kitchen/KitchenBongContainerView.java
+++ app/src/main/java/se/miun/dt170/antonsskafferi/ui/kitchen/KitchenBongContainerView.java
@@ -1,18 +1,42 @@
 package se.miun.dt170.antonsskafferi.ui.kitchen;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.LinearLayout;
 
+import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
 
 import se.miun.dt170.antonsskafferi.R;
+import se.miun.dt170.antonsskafferi.data.Item;
+import se.miun.dt170.antonsskafferi.ui.bong.BongItemView;
+import se.miun.dt170.antonsskafferi.ui.bong.BongListViewModel;
 
-public class KitchenBongContainerView extends ConstraintLayout {
-    public KitchenBongContainerView(Context context) {
+public class KitchenBongContainerView extends LinearLayout implements LifecycleObserver {
+
+    public KitchenBongContainerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public KitchenBongContainerView(Context context, LifecycleOwner lifecycle) {
         super(context);
 
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.kitchen_bong_container_view, this, true);
 
+        setOrientation(VERTICAL); // VERY IMPORTANT
+
+        BongListViewModel bongListViewModel = new BongListViewModel();
+
+        // Populate bong list on changes to the bong list view model
+        bongListViewModel.bongItems.observe(lifecycle, items -> {
+            for (Item item : items) {
+                addView(new BongItemView(getContext(), item));
+            }
+        });
     }
 }
