98b9295bb678eafb5605c8e1166f1c96b185ba71
Simon <sibe1301@student.miun.se>
2020-02-28 11:27:55 +0100

    deleted useless bloat

--- app/src/main/java/se/miun/dt170/antonsskafferi/ui/dialog/BookingTimeDialogViewModel.java
+++
@@ -1,7 +0,0 @@
-package se.miun.dt170.antonsskafferi.ui.dialog;
-
-import androidx.lifecycle.ViewModel;
-
-public class BookingTimeDialogViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
