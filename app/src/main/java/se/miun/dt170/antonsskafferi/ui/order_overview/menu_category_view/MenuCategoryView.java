d4f0c197d32d187fd6cafa8908dc259dda2d4669
mariaolsson92 <maria.olsson92@hotmail.se>
2020-03-02 14:34:23 +0100

    Fixed problems with object creation

--- app/src/main/java/se/miun/dt170/antonsskafferi/ui/order_overview/menu_category_view/MenuCategoryView.java
+++ app/src/main/java/se/miun/dt170/antonsskafferi/ui/order_overview/menu_category_view/MenuCategoryView.java
@@ -1,6 +1,7 @@
 package se.miun.dt170.antonsskafferi.ui.order_overview.menu_category_view;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 
@@ -9,9 +10,15 @@ import androidx.annotation.NonNull;
 import se.miun.dt170.antonsskafferi.R;
 
 public class MenuCategoryView extends LinearLayout {
-    public MenuCategoryView(Context context) {
+    /*public MenuCategoryView(Context context) {
         super(context);
         LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         layoutInflater.inflate(R.layout.menu_category_view, this, true);
+    }*/
+
+    public MenuCategoryView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        //LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //layoutInflater.inflate(R.layout.menu_category_view, this, true);
     }
 }
