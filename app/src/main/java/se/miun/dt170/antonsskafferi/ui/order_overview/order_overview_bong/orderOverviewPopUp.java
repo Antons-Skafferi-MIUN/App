87f2c5c718cd21c4ba22fb9381db1a676f3bb5ca
mamu1700 <mamu1700@student.miun.se>
2020-03-05 13:00:34 +0100

    design and checkboxes for order_bong_popup

--- app/src/main/java/se/miun/dt170/antonsskafferi/ui/order_overview/order_overview_bong/orderOverviewPopUp.java
+++ app/src/main/java/se/miun/dt170/antonsskafferi/ui/order_overview/order_overview_bong/orderOverviewPopUp.java
@@ -1,8 +1,12 @@
 package se.miun.dt170.antonsskafferi.ui.order_overview.order_overview_bong;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
@@ -16,13 +20,23 @@ public class orderOverviewPopUp extends Activity {
 
         setContentView(R.layout.order_bong_popup);
 
+        //storleken p popup:n
+
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
-
         int width = dm.widthPixels;
         int height = dm.heightPixels;
-
         getWindow().setLayout((int)(width*.8),(int)(height*.6));
+
+
+        //f[ bort s[ att h;rnorna 'r runda
+
+        
+
+
+        //View v = getWindow().getDecorView();
+        //v.setBackgroundResource(android.R.color.transparent);
+
 
 
     }
