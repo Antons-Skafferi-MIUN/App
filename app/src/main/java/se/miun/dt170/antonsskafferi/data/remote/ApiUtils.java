4ab2cec0f139f22f2a4e306e0de6ec5cdb9fd50a
Viktor Rosvall <virosen@yahoo.se>
2020-03-05 10:05:29 +0100

    Commented the Retrofit and Api service

--- app/src/main/java/se/miun/dt170/antonsskafferi/data/remote/ApiUtils.java
+++ app/src/main/java/se/miun/dt170/antonsskafferi/data/remote/ApiUtils.java
@@ -4,10 +4,14 @@ public class ApiUtils {
 
     public static final String BASE_URL = "http://10.250.120.152:8080/AntonAPI/webresources/";
 
-
     private ApiUtils() {
     }
 
+    /**
+     * Get the Retrofit client used to make database connections.
+     *
+     * @return a singleton Retrofit client
+     */
     public static ApiService getAPIService() {
         return RetrofitClient.getClient(BASE_URL).create(ApiService.class);
     }
