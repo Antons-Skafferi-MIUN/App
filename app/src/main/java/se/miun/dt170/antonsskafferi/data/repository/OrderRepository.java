00f71413ba00864b52a82ac546d93ffd41af49c5
Simon Bertlin <sibe1301@student.miun.se>
2020-03-10 17:05:02 +0100

    added order repo

--- app/src/main/java/se/miun/dt170/antonsskafferi/data/repository/OrderRepository.java
+++ app/src/main/java/se/miun/dt170/antonsskafferi/data/repository/OrderRepository.java
@@ -1,28 +1,57 @@
 package se.miun.dt170.antonsskafferi.data.repository;
 
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+
 import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 import se.miun.dt170.antonsskafferi.data.model.OrderRows;
+import se.miun.dt170.antonsskafferi.data.model.Orders;
 import se.miun.dt170.antonsskafferi.data.remote.ApiService;
 import se.miun.dt170.antonsskafferi.data.remote.ApiUtils;
 import se.miun.dt170.antonsskafferi.ui.dialog.TableDialogFragment;
 
 public class OrderRepository {
     private ApiService mAPIService;
+    MutableLiveData<Orders> allOrders;
+    MutableLiveData<OrderRows> allOrderRows;
 
     public OrderRepository(){
-
         mAPIService = ApiUtils.getAPIService();
-    }
-    // can use method overloading for other types.
-    // alternative builder
-
-    public Observable getOrders() {
-        return mAPIService.getOrders();
+        allOrders = new MutableLiveData<Orders>();
+        allOrderRows = new MutableLiveData<OrderRows>();
     }
 
-    public Observable<OrderRows> getOrderRows() {
+    /**
+     * Gets all orders from database and puts them in a MutableLiveData object.
+     *
+     * @return an observerable object containing all orders.
+     */
+    public MutableLiveData<Orders> getAllOrders(){
+        Log.i("func","starting func");
+        mAPIService.getOrders().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Orders>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.i("func","onComplete");
 
-        return mAPIService.getOrderRows();
+                    }
 
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.i("onError",e.toString());
+
+                    }
+
+                    @Override
+                    public void onNext(Orders orders) {
+                        Log.i("Repo",orders.toString());
+                        allOrders.setValue(orders);
+                    }
+                });
+        return allOrders;
     }
 }
